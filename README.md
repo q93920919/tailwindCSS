7bae15ff1e2fff2b3bf71549d2ecfbf37a13bc89
DESKTOP-IFE1GU4\admin <93920919@qq.com>
2022-01-04 15:17:03 +0800

    project init

--- README.md
+++ README.md
@@ -1,1 +1,46 @@
-# tailwindCSS
+### 安装 Tailwind
+
+```javascript
+npm install -D tailwindcss
+```
+
+### 创建配置文件
+
+```javascript
+npx tailwindcss init
+```
+
+### 根目录下添加tailwind.config.js
+
+```
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+```
+
+### 新建引入tailwind的tailand.css
+
+```
+@tailwind base;
+@tailwind components;
+@tailwind utilities;
+```
+
+### main.js中引入css
+
+```
+import './tailand.css'
+```
+
+
+
+### 报错 Error: Error: PostCSS plugin tailwindcss requires PostCSS 8.
+
+```
+npm uninstall tailwindcss postcss autoprefixer
+npm install -D tailwindcss@npm:@tailwindcss/postcss7-compat postcss@^7 autoprefixer@^9
+```
---
+++ src/views/Home.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="home">
+    <div class="space-y-4">
+      <div class="w-96 bg-white shadow rounded">w-96</div>
+      <div class="w-80 bg-white shadow rounded">w-80</div>
+      <div class="w-72 bg-white shadow rounded">w-72</div>
+      <div class="w-64 bg-white shadow rounded">w-64</div>
+      <div class="w-60 bg-white shadow rounded">w-60</div>
+      <div class="w-56 bg-white shadow rounded">w-56</div>
+      <div class="w-52 bg-white shadow rounded">w-52</div>
+      <div class="w-48 bg-white shadow rounded">w-48</div>
+    </div>
+    <button
+      class="bg-red-500 shadow-2xl hover:bg-red-700 text-white rounded-sm p-0.5"
+    >
+      Hover me
+    </button>
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: "Home",
+  components: {
+    // HelloWorld
+  },
+};
+</script>
